1117ebc8416ca0ba94339391b036bd7c27296c84
sakshammathur19 <skdsaksham@gmail.com>
2026-04-13 23:18:45 +0530

    11.18

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -15,7 +15,10 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", { email, password });
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        { email, password },
+      );
 
       if (res.data.success) {
         toast.success(res.data.message);
