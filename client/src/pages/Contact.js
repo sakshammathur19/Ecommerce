bffb949a10c20abbeb44c04490585147106c1980
sakshammathur19 <skdsaksham@gmail.com>
2026-04-15 01:29:55 +0530

    1.29

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/Contactus.png"
+            src={process.env.PUBLIC_URL + "/Contactus.png"}
             alt="contactus"
             style={{ width: "70%" }}
           />
